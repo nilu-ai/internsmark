3a2b8c778bd0d2b8ba23b2af8fb237b01bbd9b59
nilu-ai <79021805+nilu-ai@users.noreply.github.com>
2021-12-09 08:25:45 +0530

    Delete a.py

--- a.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-connection = sqlite3.connect("view.db")
-print("Database opened successfully")
-cursor = connection.cursor()
-#delete
-#cursor.execute('''DROP TABLE Student_Info;''')
-connection.execute("create table V (id INTEGER PRIMARY KEY ,view INTEGER  NOT NULL)")
-print("Table created successfully")
-connection.close()   
